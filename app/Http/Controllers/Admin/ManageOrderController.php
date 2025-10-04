cf6eecf409eaffc79ef37b0432c304fb3ae3d1c3
Rysul Nirob <rysulamannirob@gmail.com>
2025-10-04 10:50:57 +0600

    create page for processing confirm deliverd pending order

--- app/Http/Controllers/Admin/ManageOrderController.php
+++ app/Http/Controllers/Admin/ManageOrderController.php
@@ -4,8 +4,40 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Product;
+use Illuminate\Support\Facades\Session;
+use App\Models\Coupon;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Carbon\Carbon;
 
 class ManageOrderController extends Controller
 {
-    //
+    public function PendingOrder()
+    {
+        $allData = Order::where('status', 'Pending')->orderBy('id', 'desc')->get();
+        return view('admin.backend.order.pending_order', compact('allData'));
+    }
+    //End Method 
+    public function ConfirmOrder()
+    {
+        $allData = Order::where('status', 'confirm')->orderBy('id', 'desc')->get();
+        return view('admin.backend.order.confirm_order', compact('allData'));
+    }
+    //End Method 
+    public function ProcessingOrder()
+    {
+        $allData = Order::where('status', 'processing')->orderBy('id', 'desc')->get();
+        return view('admin.backend.order.processing_order', compact('allData'));
+    }
+    //End Method 
+    public function DeliverdOrder()
+    {
+        $allData = Order::where('status', 'deliverd')->orderBy('id', 'desc')->get();
+        return view('admin.backend.order.deliverd_order', compact('allData'));
+    }
+    //End Method 
+
+
 }
